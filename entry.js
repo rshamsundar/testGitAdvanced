131fba0f3ee822e08079ecfa0d0cb2645f870506
Rashmi <rashmis@thoughtworks.com>
2018-04-07 00:21:01 +1000

    Committing WIP to this branch until we resume

--- entry.js
+++ entry.js
@@ -1,7 +1,10 @@
+const _doSomeStuff = require('./do-some-stuff');
 const myFunction = () => {
   console.log('This is just a sample function');
   const params = {
     bucket: 'Bucket',
     key: 'Key',
   }
+  const doSomeStuff = _doSomeStuff();
+  doSomeStuff(params);
 };
