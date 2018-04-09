aebab430d0d423953a3fe3b814b91be7f4975e83
Rashmi <rashmis@thoughtworks.com>
2018-04-09 22:11:20 +1000

    Changed some implementation to find words that have same letters

--- do-some-stuff.js
+++ do-some-stuff.js
@@ -1,5 +1,6 @@
 
 module.exports = () => {
+<<<<<<< HEAD
   return (words) => {
     const checkIfTwoWordsSame = (firstWord, secondWord) => {
       const firstWordArr = Array.sort(firstWord.split(''));
@@ -11,5 +12,21 @@ module.exports = () => {
 
     checkIfTwoWordsSame(words[0], words[1]);
 
+=======
+  const result = [];
+  const checkIfSame = (first, second) => {
+    const firstArr = Array.sort(first.split(''));
+    const secondArr = Array.sort(second.split(''));
+
+    if(firstArr === secondArr){
+      console.log('Same letters in both words');
+    }
+    else {
+      console.log('Different letters in words');
+    }
+  };
+  return (words) => {
+      checkIfSame(words[0], words[1]);
+>>>>>>> Changed some implementation to find words that have same letters
   };
 }
