ae3af0f344948d59a4c1e9f6f4d9dd7b0bdba563
Rashmi <rashmis@thoughtworks.com>
2018-04-09 22:19:26 +1000

    Committing something else on the master branch

--- entry.js
+++ entry.js
@@ -4,12 +4,6 @@ const _doSomeMoreStuff = require('./do-some-more-stuff');
 //WIP WIP WIP WIP WIP
 const myFunction = () => {
   console.log('This is just a sample function');
-  const params = {
-    name: 'Name',
-    place: 'Place',
-    animal: 'Animal',
-    thing: 'Thing'
-  }
   const doSomeStuff = _doSomeStuff();
   doSomeStuff(['sole', 'lose']);
   const doSomeMoreStuff = _doSomeMoreStuff();
