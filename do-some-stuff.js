51f5aca4eb26fefd886734e9b644efef5b633401
Rashmi <rashmis@thoughtworks.com>
2018-04-11 08:13:39 +1000

    Adding some impl to check if two words are same

--- do-some-stuff.js
+++ do-some-stuff.js
@@ -1,17 +1,13 @@
 module.exports = () => {
-  const result = [];
-  const checkIfSame = (first, second) => {
-    const firstArr = Array.sort(first.split(''));
-    const secondArr = Array.sort(second.split(''));
-
-    if(firstArr === secondArr){
-      console.log('Same letters in both words');
-    }
-    else {
-      console.log('Different letters in words');
-    }
-  };
   return (words) => {
-      checkIfSame(words[0], words[1]);
+    const checkIfTwoWordsSame = (firstWord, secondWord) => {
+      const firstWordArr = Array.sort(firstWord.split(''));
+      const secondWordArr = Array.sort(secondWord.split(''));
+
+      return firstWordArr === secondWordArr;
+      
+    };
+
+    checkIfTwoWordsSame(words[0], words[1]);
   };
 }
